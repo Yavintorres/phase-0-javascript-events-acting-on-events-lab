cf867f3d7e2fd676038d18942702804f578868a4
Yavin Torres <yavintorres@gmail.com>
2023-04-18 07:04:22 -0700

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-// Your code here
+// Move Dodger Left Code
+const dodger = document.getElementById("dodger")
+
+function moveDodgerLeft() {
+    const leftNumbers = dodger.style.left.replace("px", "")
+    const left = parseInt(leftNumbers, 10)
+    if (left > 0) {
+        dodger.style.left =`${left -1}px`
+    }
+}
+document.addEventListener("keydown", function (e) {
+    if (e.key === "ArrowLeft") {
+        moveDodgerLeft()
+    }
+})
+
+//Move Dodger Right Code
+function moveDodgerRight() {
+    const leftNumbers = dodger.style.left.replace("px", "")
+    const left = parseInt(leftNumbers, 10)
+    if (left < 360) {
+        dodger.style.left = `${left + 1}px`
+    }
+}
+document.addEvenetListener("keydown", function (e) {
+    if (e.key === "ArrowRight") {
+        moveDodgerRight()
+    }
+})
